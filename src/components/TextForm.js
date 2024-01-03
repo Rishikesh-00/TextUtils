45c14f4c8adba92de218119910d5dbbea5b57565
Rishikesh--0 <keshrishi1789@gmail.com>
2024-01-03 19:30:26 +0530

    git commit -m "upper case completed"

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,16 +9,15 @@ export default function TextForm(props) {
     const handelOnChange=(_)=>{
         setText(_.target.value)
     }
-    const [text,setText] = useState('Enter text here');
+    const [text,setText] = useState('');
   return (
     <div>
         <div className="form-group">
             <h1>{props.heading}</h1>
-          {/* <label for="MyBox">Example textarea</label> */}
           <textarea
             className="form-control"
             id="MyBox"
-            rows="8" value={text} onChange={handelOnChange}
+            rows="8" value={text} onChange={handelOnChange} placeholder="Enter Text Here"
           ></textarea> <br />
           <button type="button" className="btn btn-primary" onClick={handelUpClick}>Convert to upper case</button>
         </div>
