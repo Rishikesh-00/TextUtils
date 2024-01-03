3c79c63dd0274685657da22fc7c98ac0c5302f08
Rishikesh--0 <keshrishi1789@gmail.com>
2024-01-03 22:11:04 +0530

    git commit -m"features added -lower case ,text summery and preview"

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,23 +4,41 @@ import React,{useState} from "react";
 
 export default function TextForm(props) {
     const handelUpClick=()=>{
+      if (text.length===0)
+        alert("Empty TextField")
+      else
         setText(text.toUpperCase())
+    }
+    const handelowClick=()=>{
+      if (text.length===0)
+        alert("Empty TextField")
+      else
+        setText(text.toLowerCase())
     }
     const handelOnChange=(_)=>{
         setText(_.target.value)
     }
     const [text,setText] = useState('');
   return (
-    <div>
-        <div className="form-group">
+    <>
+   
+        <div className="container">
             <h1>{props.heading}</h1>
           <textarea
             className="form-control"
             id="MyBox"
-            rows="8" value={text} onChange={handelOnChange} placeholder="Enter Text Here"
+            rows="8" value={text} onChange={handelOnChange} placeholder="Enter Text Here" 
           ></textarea> <br />
-          <button type="button" className="btn btn-primary" onClick={handelUpClick}>Convert to upper case</button>
+          <button type="button " className="btn btn-primary mx-2" onClick={handelUpClick}>Convert to upper case</button>
+          <button type="button " className="btn btn-primary mx-2" onClick={handelowClick}>Convert to lowercase case</button>
         </div>
-    </div>
+      <div className="container my-4">
+        <h1>Your text summery</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.08 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
