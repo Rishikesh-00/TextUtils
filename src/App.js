76797dfc92ecaf07f93a58fdaa487b0f8eda56c2
Rishikesh--0 <keshrishi1789@gmail.com>
2024-01-04 17:33:48 +0530

    commit -m "about.js comented "

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-// import logo from './logo.svg';
 import "./App.css";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
-import About from "./components/About";
+import TextForm from "./components/TextForm";
+// import About from "./components/About";
 function App() {
   return (
     <>
       {/* <Navbar /> */}
       <Navbar title="TextUtils" about="About Us" />
-      {/* <div className="container">
-      <TextForm heading="Enter the text to analyse"/>
-      </div> */}
       <div className="container">
-      <About/>
+      <TextForm heading="Enter the text to analyse"/>
       </div>
+      {/* <div className="container">
+      <About/>
+      </div> */}
     </>
   );
 }
